42a034068cdbd25c13465776a2274cd4c3fff5d3
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-03-20 09:26:06 +0000

    Update HomeContent.js

--- src/Components/HomeContent.js
+++ src/Components/HomeContent.js
@@ -12,7 +12,7 @@ const HomeContent = () => {
         </h1>
         <p>A Professional Web Developer</p>
         <a
-          href="https://drive.google.com/file/d/15fOrPO7bCFAU_g907gTyfzAkPPJ-gWLV/view"
+          href="https://drive.google.com/file/d/1HEg-CD2FBsTo0KP8J2orpr5sp8-idGZR/view?usp=share_link"
           target="_blank"
           rel="noreferrer"
         >
